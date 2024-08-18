6e6d5c27e4744a2cb51e1f26ed07781798f938aa
Redon ALLA <ralla@bkt.com.al>
2024-08-18 16:43:01 +0200

    .

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
   url: 'https://redonalla.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ra-framework-docks',
+  baseUrl: '/ra-framework-docks/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
